c1c3b5f61c91dca1eb30423d17835a955e98477b
tanyavibe <tanyavibe@gmail.com>
2022-06-20 14:23:21 +0300

    Исправили ошибки

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,11 +34,12 @@
 // }
 
 // - список товаров в заказе
+
 function sendRequest(name, phone, address, goods, sum) {
     let data = {
-        client: {name, ' ', phone},
+        client: {name, phone},
         order: {
-            address: 'ул. ' + street + ', дом ' + house + ', ' + entrance + ' подъезд' + floor + ' этаж' + ', кв.' + flat,
+            address: street + house + entrance + floor + flat,
             sum
         },
         goods: []
