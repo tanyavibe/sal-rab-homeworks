3315ce46df2d9bd30188f2049ba0482b9f6b0be0
tanyavibe <tanyavibe@gmail.com>
2022-06-24 09:50:45 +0300

    Убрали двоеточия у title и count

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,8 +39,7 @@ function sendRequest (name, phone, address, goods, sum) {
     let data = {
         client: (name + phone),
         order: {
-            address = (street, house, entrance, floor, flat),
-            address: ("ул. " + street + ", дом " + house + ", " + entrance + " подъезд, " + floor + " этаж, " + "кв " + flat), 
+            address: ("ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat), 
             sum 
         },
         goods: []
@@ -49,7 +48,7 @@ function sendRequest (name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i++) {
-        data.goods.push(title: goods[i].title, count: goods[i].count);
+        data.goods.push(title, goods[i].title, count, goods[i].count);
     }
     
     let jsonData = JSON.stringify({data});
